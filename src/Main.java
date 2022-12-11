6aec1db01e1cbf4a371912d1cab07f3f22d241eb
Muhammed Solmaz <muhammed.slmz@gmail.com>
2022-12-11 05:27:12 +0300

    2. examples

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner inp=new Scanner(System.in);
-        int sum=0,num;
-        do {
-            System.out.print("Enter a number: ");
-            num = inp.nextInt();
-            if(num%4==0)
-            {
-                sum+=num;
-            }
 
-        }while (num%2==0);
-        System.out.println(sum);
 
+        int num=inp.nextInt();
+
+
+        for(int i=4;i <num;i*=4)
+        {
+            if(i==4) System.out.println("0");
+            System.out.println(i);
+        }
+
+        for(int j=5; j<num;j*=5)
+        {
+            if(j==5) System.out.println("0");
+            System.out.println(j);
+        }
     }
 }
