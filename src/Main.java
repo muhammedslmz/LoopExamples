733446ba81a007002088807a10160518471ed55d
Muhammed Solmaz <muhammed.slmz@gmail.com>
2022-12-11 05:28:13 +0300

    3th example.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+    {
         Scanner inp=new Scanner(System.in);
 
 
